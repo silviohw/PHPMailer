5d235bfd7b8b0a3d6a381369e8e59a5c170cf81d
Silvio Pardo <silviopardo@silviopardo.it>
2020-05-20 17:09:46 +0200

    Initial  of filecore class
    Signed-off-by: Silvio Pardo <silviopardo@silviopardo.it>

---
+++ src/FileCore.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class for detect and use the correct mimetype when sending e-mail
+ * Date: 20/05/20
+ * Time: 17.06
+ */
+
+namespace PHPMailer\FileCore;
+
+
+class FileCore
+{
+
+}
